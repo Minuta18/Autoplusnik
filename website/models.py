6e428a4629aff14a3499e9958ffc71422b4205e6
GitHubik1 <ig.samsonov10@yandex.ru>
2023-04-28 22:02:33 +0300

    test commit

--- website/models.py
+++ website/models.py
@@ -15,7 +15,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     role = db.Column(db.SmallInteger, default=Permissions.no_permissions.value)
-    password = db.Column(db.String(100))
+    password = db.Column(db.String(100)) #
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
